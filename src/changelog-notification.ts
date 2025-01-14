abb7db6672ea8300ec0049f8ade313f7662fb76c
Marco Krikke <marco.krikke@semestry.com>
2025-01-14 22:01:42 +0100

    Improve change notification

--- src/changelog-notification.ts
+++ src/changelog-notification.ts
@@ -1,7 +1,11 @@
-import type {Block, DividerBlock, HeaderBlock, SectionBlock} from '@slack/types'
+import type {ActionsBlock, Block, Button, ContextBlock, DividerBlock, HeaderBlock, PlainTextElement} from '@slack/types'
 import type {OauthV2AccessResponse} from '@slack/web-api'
 import axios from 'axios'
 import {markdownToBlocks} from '@tryfabric/mack'
+
+interface Author {
+  login: string
+}
 
 interface Repository {
   repo: string
@@ -12,6 +16,7 @@ interface Release {
   html_url: string
   name: string
   body: string
+  author: Author
 }
 
 interface ChangelogParameters {
@@ -29,22 +34,44 @@ export async function notifyChangelog({
     type: 'header',
     text: {
       type: 'plain_text',
-      text: `🎉 [CHANGELOG]: ${release.name}`
+      text: `🎉 ${repo.owner}/${repo.repo} ${release.name}`
     }
   }
-  const linkBlock: SectionBlock = {
-    type: 'section',
+
+  const releaseButton: Button = {
+    type: 'button',
     text: {
-      type: 'mrkdwn',
-      text: `<${release.html_url}>`
-    }
+      type: 'plain_text',
+      text: 'View release',
+    },
+    url: `${release.html_url}`
   }
+
+  const actionBlock: ActionsBlock = {
+    type: 'actions',
+    elements: [
+      releaseButton
+    ]
+  }
+
+  const author: PlainTextElement = {
+    type: 'plain_text',
+    text: `Author: ${release.author.login}`
+  }
+
+  const contextBlock: ContextBlock = {
+    type: 'context',
+    elements: [
+      author
+    ]
+  }
+
   const dividerBlock: DividerBlock = {type: 'divider'}
 
   const bodyBlocks: Block[] = await markdownToBlocks(release.body)
 
   return await axios.post(slackWebhookUrl, {
     text: `${release.name} has been released in ${repo.owner}/${repo.repo}`,
-    blocks: [introBlock, linkBlock, dividerBlock, ...bodyBlocks]
+    blocks: [introBlock, ...bodyBlocks, dividerBlock, actionBlock, contextBlock]
   })
 }
