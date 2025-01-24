21ab25d850bbeb3bd6339345af3d2e36909fd2b3
Marco Krikke <marco.krikke@semestry.com>
2025-01-24 20:58:22 +0100

    Change layout of release message

--- src/changelog-notification.ts
+++ src/changelog-notification.ts
@@ -1,7 +1,19 @@
-import type {Block, DividerBlock, HeaderBlock, SectionBlock} from '@slack/types'
+import type {
+  ActionsBlock,
+  Block,
+  Button,
+  ContextBlock,
+  DividerBlock,
+  HeaderBlock,
+  PlainTextElement
+} from '@slack/types'
 import type {OauthV2AccessResponse} from '@slack/web-api'
 import axios from 'axios'
 import {markdownToBlocks} from '@tryfabric/mack'
+
+interface Author {
+  login: string
+}
 
 interface Repository {
   repo: string
@@ -12,6 +24,7 @@ interface Release {
   html_url: string
   name: string
   body: string
+  author: Author
 }
 
 interface ChangelogParameters {
@@ -29,22 +42,42 @@ export async function notifyChangelog({
     type: 'header',
     text: {
       type: 'plain_text',
-      text: `🎉 [CHANGELOG]: ${release.name}`
+      text: `:tada: ${repo.owner}/${repo.repo} ${release.name}`,
+      emoji: true
     }
   }
-  const linkBlock: SectionBlock = {
-    type: 'section',
+
+  const releaseButton: Button = {
+    type: 'button',
     text: {
-      type: 'mrkdwn',
-      text: `<${release.html_url}>`
-    }
+      type: 'plain_text',
+      text: 'View release'
+    },
+    url: `${release.html_url}`
   }
+
+  const actionBlock: ActionsBlock = {
+    type: 'actions',
+    elements: [releaseButton]
+  }
+
+  const author: PlainTextElement = {
+    type: 'plain_text',
+    text: `:technologist: Author: ${release.author.login}`,
+    emoji: true
+  }
+
+  const contextBlock: ContextBlock = {
+    type: 'context',
+    elements: [author]
+  }
+
   const dividerBlock: DividerBlock = {type: 'divider'}
 
   const bodyBlocks: Block[] = await markdownToBlocks(release.body)
 
   return await axios.post(slackWebhookUrl, {
     text: `${release.name} has been released in ${repo.owner}/${repo.repo}`,
-    blocks: [introBlock, linkBlock, dividerBlock, ...bodyBlocks]
+    blocks: [introBlock, ...bodyBlocks, dividerBlock, actionBlock, contextBlock]
   })
 }
